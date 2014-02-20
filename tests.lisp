79e148d9bcadc03e97148e9cccb12d9e5aef381d
flpa <flpa.dev@zoho.com>
2014-02-20 20:33:58 +0000

    Create tests.lisp
    test file with some little experiments to get a feel for the challenge

---
+++ tests.lisp
@@ -0,0 +1,21 @@
+
+;start of actual draft... will need loop
+
+(let ((x (read)) (y (read)))
+     
+ )
+
+
+; experiments on reading the input for this challenge
+
+(defun botread ()
+   (format t "~a~%" (read)))
+
+
+(botread) ; 0
+(botread) ; 0
+(botread) ; line1
+(botread) ; line2
+(botread) ; line3
+(botread) ; line4
+(botread) ; line5
