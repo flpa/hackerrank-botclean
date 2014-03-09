7da17a3a36d320776771b83cc349c0271fbaa096
flpa <flpa.dev@zoho.com>
2014-03-09 20:57:21 +0100

    Add permutate function

--- pathfinder.lisp
+++ pathfinder.lisp
@@ -6,3 +6,10 @@
 ; -- for each permutation, generate instruction list, minimizing the path length
 ; -- the winning instruction list is our instruction list
 ; - pop the next instruction from the list
+
+
+(defun permutate (list)
+  (if (null list) '(())
+  (mapcan #'(lambda (x)
+    (mapcar #'(lambda (y) (cons x y))
+      (permutate (remove x list :count 1)))) list)))
